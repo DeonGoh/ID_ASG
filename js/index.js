982650e3763359a617775605a31398b2565e5e37
DeonGoh <61679463+DeonGoh@users.noreply.github.com>
2022-11-27 01:05:08 +0800

    INDEX PAGE Changed Products in Index page to categories Added scrollable testimonials
    INITIAL PAGES
    Added initial testimonies, cart, contact, promotions, products page
    
    PRODUCTS PAGE
    Added category list in products page
    Added products listing in products page
    Added page number in products page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
 function imageChange(img) {
   document.getElementById("selected-image").src = img;
-}
+};
+
+function Load(){
+  var slider = document.querySelector(".testimonies");
+  console.log(slider);
+  //on mouse click
+  let mouseDown = false;
+  let startX, scrollLeft;
+  //when mouse down
+  let startDragging = function(e){
+    console.log("startDragging");
+    mouseDown = true;
+    //the movement to move left
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  };
+
+  //attach to mouseUp Event
+  let stopDragging = function(event){
+    console.log("stopDragging");
+    mouseDown = false;
+  };
+
+  slider.addEventListener('mousemove', (e) => {
+    e.preventDefault();
+    console.log(mouseDown);
+    if(!mouseDown) { return; }
+    console.log("onmousemove");
+    const x = e.pageX - slider.offsetLeft;
+    const scroll = x - startX;
+    console.log(x);
+    slider.scrollLeft = scrollLeft - scroll;
+  });
+  
+  // Add the event listeners
+  slider.addEventListener('mousedown', startDragging, false);
+  slider.addEventListener('mouseup', stopDragging, false);
+  slider.addEventListener('mouseleave', stopDragging, false);
+};
